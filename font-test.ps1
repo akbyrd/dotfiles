4f85b53f04ab21c961f53f9c72d799c1d84a003f
Adam Byrd <adam1.byrd@gmail.com>
2023-10-08 13:08:28 -0700

    Fix filename for Install-Font

--- font-test.ps1
+++ font-test.ps1
@@ -2,7 +2,7 @@ Set-ExecutionPolicy Bypass
 $installFontUrl = "https://raw.githubusercontent.com/ralish/PSWinGlue/master/Scripts/Install-Font.ps1"
 Invoke-WebRequest $installFontUrl -OutFile "Install-Font.ps1"
 
-$fontUrl = "https://api.github.com/repos/ryanoasis/nerd-fonts/contents/patched-fonts/Inconsolata"
+$fontUrl = "https://api.github.com/repos/ryanoasis/nerd-fonts/contents/patched-fonts/Agave"
 $headers = @{ Accept = "application/vnd.github.raw" }
 $result = iwr -UseBasicParsing -Headers $headers -Uri $fontUrl
 $fontFiles = ConvertFrom-JSON $result.Content
@@ -11,7 +11,7 @@ foreach ($fontFile in $fontFiles)
 	if ($fontFile.name.EndsWith(".ttf"))
 	{
 		Invoke-WebRequest -Uri $fontFile.download_url -OutFile $fontFile.name
-		.\Install-Font.ps1 $font.file -Scope User -Method Shell
+		.\Install-Font.ps1 -Verbose $fontFile.name -Scope User -Method Shell
 		Remove-Item $fontFile.name
 	}
 }
